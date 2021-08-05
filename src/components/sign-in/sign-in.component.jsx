6a07a26a72fa3bdbb6b39ab3d03161cf6b897bc6
Tomas Heed <tomas@ekkono.ai>
2021-08-05 15:26:34 +0200

    End of firebase section

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -4,7 +4,7 @@ import './sign-in.styles.scss'
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import {signInWithGoogleMethod} from '../../firebase/firebase.utils'
+import {auth, signInWithGoogleMethod} from '../../firebase/firebase.utils'
 
 class SignIn extends React.Component {
     constructor(props){
@@ -17,12 +17,22 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
-        this.setState({
-            email: '',
-            password: ''
-        });
+
+        const {email, password } = this.state;
+
+        try{
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({
+                email: '',
+                password: ''
+            });
+    
+        }catch(error){
+            console.log(error);
+        }
+
     }
 
     handleChange = event => {
@@ -42,7 +52,7 @@ class SignIn extends React.Component {
                     <FormInput name='password' type='password' value={this.state.password} onChange={this.handleChange} required label='Password'/>
                     <div className='buttons'>
                         <CustomButton type="submit" >Sign In</CustomButton>
-                        <CustomButton onClick={signInWithGoogleMethod} isGoogleSignIn >Sign in with Google</CustomButton>
+                        <CustomButton type="button" onClick={signInWithGoogleMethod} isGoogleSignIn >Sign in with Google</CustomButton>
                     </div>
                 </form>
             </div>
